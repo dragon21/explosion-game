852cdea412e945ddbd7adab5c1c83a77ab43f6a6
PAPADOPOULOS KONSTANTINOS <kospaps@hotmail.com>
2022-12-22 15:08:50 +0200

    with sound

--- main.ts
+++ main.ts
@@ -1,10 +1,39 @@
 input.onButtonPressed(Button.A, function () {
 	
 })
-let timer = randint(5, 15)
+let timer = randint(20, 35)
 basic.showIcon(IconNames.SmallDiamond)
 while (timer > 0) {
     timer += -1
-    basic.pause(1000)
+    basic.pause(1500)
+    music.playSoundEffect(music.createSoundEffect(
+    WaveShape.Square,
+    200,
+    1,
+    255,
+    0,
+    50,
+    SoundExpressionEffect.None,
+    InterpolationCurve.Curve
+    ), SoundExpressionPlayMode.UntilDone)
 }
-basic.showIcon(IconNames.Chessboard)
+music.playSoundEffect(music.createSoundEffect(
+WaveShape.Noise,
+500,
+499,
+255,
+0,
+3952,
+SoundExpressionEffect.None,
+InterpolationCurve.Linear
+), SoundExpressionPlayMode.InBackground)
+for (let index = 0; index < 5; index++) {
+    basic.showIcon(IconNames.Chessboard)
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+}
