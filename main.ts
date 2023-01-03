d2e41e6b532f6791d5f9ae1758c216e4510404a5
PAPADOPOULOS KONSTANTINOS <kospaps@hotmail.com>
2023-01-03 11:56:14 +0200

    add if for the game

--- main.ts
+++ main.ts
@@ -1,40 +1,24 @@
-input.onButtonPressed(Button.A, function () {
-	
+let timer = randint(1, 4)
+basic.forever(function () {
+    if (timer > 0) {
+        basic.showIcon(IconNames.Square)
+        timer += -1
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        basic.showIcon(IconNames.SmallSquare)
+        basic.pause(200)
+    } else {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 500, 499, 255, 0, 1500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        for (let index = 0; index < 5; index++) {
+            basic.showIcon(IconNames.Chessboard)
+            basic.showLeds(`
+                # . # . #
+                . # . # .
+                # . # . #
+                . # . # .
+                # . # . #
+                `)
+        }
+        basic.pause(2000)
+        timer = randint(1, 4)
+    }
 })
-let timer = randint(20, 35)
-while (timer > 0) {
-    basic.showIcon(IconNames.SmallDiamond)
-    timer += -1
-    basic.pause(1500)
-    music.playSoundEffect(music.createSoundEffect(
-    WaveShape.Square,
-    200,
-    1,
-    255,
-    0,
-    50,
-    SoundExpressionEffect.None,
-    InterpolationCurve.Curve
-    ), SoundExpressionPlayMode.InBackground)
-    basic.showIcon(IconNames.SmallSquare)
-}
-music.playSoundEffect(music.createSoundEffect(
-WaveShape.Noise,
-500,
-499,
-255,
-0,
-3952,
-SoundExpressionEffect.None,
-InterpolationCurve.Linear
-), SoundExpressionPlayMode.InBackground)
-for (let index = 0; index < 5; index++) {
-    basic.showIcon(IconNames.Chessboard)
-    basic.showLeds(`
-        # . # . #
-        . # . # .
-        # . # . #
-        . # . # .
-        # . # . #
-        `)
-}
