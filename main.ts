cdfa563f11738767322208ca2f67c52c44e80c75
PAPADOPOULOS KONSTANTINOS <kospaps@hotmail.com>
2023-01-20 16:23:41 +0200

    clean

--- main.ts
+++ main.ts
@@ -1,25 +1,71 @@
-let timer = randint(1, 4)
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.showString("START")
+let XRONOS = randint(5, 30)
+music.setVolume(255)
 basic.forever(function () {
-    if (timer > 0) {
-        basic.showIcon(IconNames.Square)
-        timer += -1
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
-        basic.showIcon(IconNames.SmallSquare)
-        basic.pause(200)
+    if (XRONOS > 0) {
+        XRONOS += -1
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1600, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.clearScreen()
+        basic.pause(300)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1600, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(300)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1600, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(300)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1600, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(300)
     } else {
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 500, 499, 255, 0, 1500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
-        for (let index = 0; index < 5; index++) {
-            basic.showIcon(IconNames.Chessboard)
-            basic.pause(100)
-            basic.showLeds(`
-                # . # . #
-                . # . # .
-                # . # . #
-                . # . # .
-                # . # . #
-                `)
-        }
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 523, 1, 255, 0, 2000, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.InBackground)
+        basic.showIcon(IconNames.Chessboard)
+        basic.clearScreen()
         basic.pause(2000)
-        timer = randint(1, 4)
+        basic.showLeds(`
+            . . # # .
+            . # . . .
+            . # # # .
+            . # . . .
+            . . # # .
+            `)
+        basic.clearScreen()
+        basic.pause(300)
+        basic.showString("you lost. play again. ")
+        basic.showNumber(3)
+        basic.showNumber(2)
+        basic.showNumber(1)
+        basic.showString("START")
+        XRONOS = randint(5, 30)
     }
 })
